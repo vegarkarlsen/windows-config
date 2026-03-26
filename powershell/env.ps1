41f9630add289561732590ae44ead2c940e96283
Vegar Karlsen <vegar.karlsen@kartverket.no>
2026-03-26 16:06:50 +0100

    Add to path.

--- powershell/env.ps1
+++ powershell/env.ps1
@@ -1,5 +1,15 @@
 
+$env:EDITOR = "nvim-qt"
+
 # Add to path
 $env:PATH += ";C:\Program Files\LLVM\bin"
 $env:PATH += ";C:\Program Files\Vim\vim91"
 $env:PATH += ";C:\Program Files\Neovim\bin"
+$env:PATH += ";$HOME\.local\var\nvim-win64\bin"
+
+# User executables
+$env:PATH += ";$HOME\.local\bin"
+
+# Starship
+$env:STARSHIP_CONFIG = "$HOME\Documents\WindowsPowerShell\windows-config\starship\starship.toml"
+
