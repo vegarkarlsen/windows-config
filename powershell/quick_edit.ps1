4fd6a183e7c85edd2d2eabad878f2496210dfa88
Vegar Karlsen <vegar.karlsen@kartverket.no>
2026-03-26 15:56:01 +0100

    Seperate quick edit commands into its own file.

---
+++ powershell/quick_edit.ps1
@@ -0,0 +1,42 @@
+
+
+function Change-Folder-Open-file {
+        param($file)
+
+        Push-Location (Split-Path $file)
+        Invoke-Expression "$env:EDITOR $file"
+        Pop-Location
+    }
+
+# Make it easy to edit this profile once it's installed
+function Edit-Profile {
+    $file = $profile.CurrentUserAllHosts
+    # if ($host.Name -match "ise") {
+    #     $psISE.CurrentPowerShellTab.Files.Add($profile.CurrentUserAllHosts)
+    # }
+
+    Change-Folder-Open-file $file
+    . $file
+
+}
+
+function Edit-alias {
+    $alias_file = "$powershell_config/aliases.ps1"
+    Change-Folder-Open-file $alias_file
+    . $alias_file
+}
+
+function Edit-nvim {
+    $nvim_folder = "$env:LocalAppData/nvim"
+    Change-Folder-Open-file $nvim_folder
+}
+
+function Edit-penv {
+        $env_file = "$powershell_config/env.ps1"
+        # nvim "$env_file"
+        Change-Folder-Open-file $env_file
+        . $env_file
+    }
+
+
+Set-Alias malias Edit-alias
