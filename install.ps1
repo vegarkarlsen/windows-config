0b1cbbfc1cd4e403cc529959447df0f455d409a6
Vegar Karlsen <Vegar.Karlsen@kartverket.no>
2026-05-11 09:10:02 +0200

    variable typo
    Forgot '$' in variable

--- install.ps1
+++ install.ps1
@@ -5,7 +5,7 @@ $powershell_config = "$profile_folder/windows-config/powershell"
 
 # link profile script. Use Hardlink instead of Smbolic due to symboic requires admin access
 # New-Item -Path "$profile_folder/profile.ps1" -ItemType SymbolicLink -Value "$powershell_config/profile.ps1" -Force
-New-Item -Path "$profile_folder/profile.ps1" -ItemType HardLink -Value "powershell_config/profile.ps1"
+New-Item -Path "$profile_folder/profile.ps1" -ItemType HardLink -Value "$powershell_config/profile.ps1"
 
 # Creat windows terminal junction for Fragments
 New-Item -ItemType Junction -Path "$ENV:LOCALAPPDATA/Microsoft/Windows Terminal/Fragments" -Target "$profile_folder/windows-config\WindowsTerminal\Fragments\"
